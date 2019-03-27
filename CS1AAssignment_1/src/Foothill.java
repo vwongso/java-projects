f3be79fb948af7a2a58fa4f602f4c71265485adb
vwongso <31841692+vwongso@users.noreply.github.com>
2019-03-26 19:51:36 -0700

    Add files via upload

---
+++ CS1AAssignment_1/src/Foothill.java
@@ -0,0 +1,23 @@
+public class Foothill
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello Professor!");
+      System.out.println("My family name is Wongso");
+      System.out.println("My student ID is 20119854");
+      System.out.println("I noticed in the syllabus that all students must " + 
+         "post at least one question, comment or answer in the public forum " + 
+         "every week.");
+      
+   }
+}
+
+/* ---------------- sample run --------------------------------
+
+Hello Professor!
+My family name is Wongso
+My student ID is 20119854
+I noticed in the syllabus that all students must post at least one question, com
+ment or answer in the public forum every week.
+
+------------------------------------------------------------- */
